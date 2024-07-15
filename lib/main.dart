bcf747e63e5fc0260aa0ac3c17bfab9ed1d71d13
b-suryaraj <suryaraj.srb112@gmail.com>
2024-07-15 23:57:00 +0530

    SplashScreen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
-import 'package:chat_hive/screens/auth/login_screen.dart';
+import 'package:chat_hive/screens/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'firebase_options.dart';
 
 
 late Size mq;
 
 void main() {
-  _initializeFirebase();
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown])
+  .then((value) {
+    _initializeFirebase();
+    runApp(const MyApp());
+    }
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -31,7 +38,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const LoginScreen(),
+      home: const SplashScreen(),
     );
   }
 }
