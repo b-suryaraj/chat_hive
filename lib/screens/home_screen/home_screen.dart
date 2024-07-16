10599b8bad2b171e3b958e4567adb8bd952e3a8c
b-suryaraj <suryaraj.srb112@gmail.com>
2024-07-16 19:35:18 +0530

    firebase firestore connected and homescreen card ui added

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:chat_hive/api/apis.dart';
+import 'package:chat_hive/main.dart';
+import 'package:chat_hive/widgets/chat_user_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -34,6 +36,15 @@ class _HomeScreenState extends State<HomeScreen>
           await GoogleSignIn().signOut();
         }, child: const Icon(Icons.add_comment_rounded)),
       ),
+    
+      body: ListView.builder(
+        itemCount:16,
+        padding:EdgeInsets.only(top: mq.height*.01),
+        physics: BouncingScrollPhysics(),
+        itemBuilder:(context,index){
+          return const ChatUserCard();
+        } 
+      )
     );
   }
 }
--- lib/widgets/chat_user_card.dart
+++ lib/widgets/chat_user_card.dart
@@ -1,4 +1,7 @@
+import 'package:chat_hive/main.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ChatUserCard extends StatefulWidget{
   const ChatUserCard({super.key});
@@ -10,6 +13,22 @@ class ChatUserCard extends StatefulWidget{
 class _ChatUserCardState extends State<ChatUserCard>{
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      margin:EdgeInsets.symmetric(horizontal: mq.width*.03, vertical:4),
+      elevation: 0.5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: InkWell(
+        onTap: (){},
+        child:const ListTile(
+          leading: CircleAvatar(child:Icon(CupertinoIcons.person)),
+          title : Text('Demo USer'),
+          subtitle: Text('Last user message', maxLines: 1),
+          trailing: Text(
+            '12:00 PM',
+            style: TextStyle(color: Colors.black54),  
+          ),
+        )
+      )
+    );
   }
 }
