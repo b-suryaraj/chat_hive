b3a4cf6e75fa5aca7784cd6a5e92f18d02755b2b
b-suryaraj <suryaraj.srb112@gmail.com>
2024-07-18 07:01:54 +0530

    profile screen with features along with dynamic updation completed

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -1,13 +1,18 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:chat_hive/models/chat_user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class APIs {
   static FirebaseAuth auth = FirebaseAuth.instance;
 
   static FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  static FirebaseStorage storage = FirebaseStorage.instance;
+  
   static late ChatUser me;
   static User get user => auth.currentUser!;
   
@@ -67,4 +72,50 @@ class APIs {
     });
   }
 
+  // static Future<void> updateProfilePicture(File file) async {
+   
+  //   final ext = file.path.split('.').last;
+  //   log('Extension: $ext');
+
+  //   final ref = storage.ref().child('profile_pictures/${user.uid}.$ext');
+
+  //   await ref
+  //       .putFile(file, SettableMetadata(contentType: 'image/$ext'))
+  //       .then((p0) {
+  //     log('Data Transferred: ${p0.bytesTransferred / 1000} kb');
+  //   });
+
+  //   me.image = await ref.getDownloadURL();
+  //  await firestore.collection('users').doc(user.uid).update({
+  //     'image': me.image,
+  //   });
+  // }
+
+  static Future<void> updateProfilePicture(File file) async {
+  try {
+    final ext = file.path.split('.').last;
+    log('Extension: $ext');
+
+    //storage file ref with path
+    final ref = storage.ref().child('profile_pictures/${user.uid}.$ext');
+
+    //uploading image
+    await ref
+        .putFile(file, SettableMetadata(contentType: 'image/$ext'))
+        .then((p0) {
+      log('Data Transferred: ${p0.bytesTransferred / 1000} kb');
+    });
+
+    //updating image in firestore database
+    me.image = await ref.getDownloadURL();
+    await firestore
+        .collection('users')
+        .doc(user.uid)
+        .update({'image': me.image});
+
+    log('Profile picture updated successfully!');
+  } catch (e) {
+    log('Error updating profile picture: $e');
+  }
+}
 }
